410dba0e545f022fc34df5a757cb66ac16faa737
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-07-22 18:07:06 -0400

    omdb command for liri completed

--- liri.js
+++ liri.js
@@ -7,73 +7,69 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 
-
 // Store all of the argument a user inputs into an array
 var procArr = process.argv;
 var liriCommand = process.argv[2];
-var searchTerm = process.argv.slice(3).join(" ");
 
-// Movie or song variable
-var movOrSong = "";
+// Movie or song variable is search term according to case
+var searchTerm = searchTerm = process.argv.slice(3).join(" ");
 
+if (!procArr[3]) {
+    searchTerm = "The Sign";
+}
 
 //-------------------------GLOBAL FUNCTIONS------------------------//
 
-// Attaches multiple word arguments together into movOrSong global variable
-// var getMovOrSong = function () {
-//     procArr.slice(3).join(" ");
-// }
-// getMovOrSong();
-
 // Use of spotify API to execute spotify song search case command
-// take the movie or song varriable and use it as the search parameter in the spotify API
-// result will display Artist(s), song's name, A preview link of the song, and album that the song is from
 //If no song is provided then your program will default to "The Sign" by Ace of Base.
 var spotifyFn = function (searchTerm) {
     // call to spotify website with a promise
+    // take the movie or song varriable and use it as the search parameter in the spotify API
     spotify.search({
             type: "track",
             query: searchTerm,
-            limit: 3
+            limit: 5
         },
         function (error, data) {
             if (error) {
                 return console.log('Error occurred: ' + error);
             }
-            // var data = JSON.stringify(data, null, 2)
             var dataTra = data.tracks.items;
-            // for(keys in data){
-            //     console.log(JSON.parse(data))
-            for (var i = 0; i < dataTra.length; i++) {
-
-                console.log(dataTra)
-                console.log("=========================")
-                console.log(dataTra[i])
-                
-                var artists = dataTra[i].artists.map(function (artist) {
+            if (!procArr[3]) {
+                var artists = dataTra[2].artists.map(function (artist) {
                     return artist.name
                 });
-
-                var song = searchTerm.toUpperCase() ;
-                var album = dataTra[i].name;
-                var link = dataTra[i].href;
-
-
                 console.log(`
+          ===============================  Your Default Result  ==============================      
+
+                Artist: ${artists}
+                Song: ${dataTra[2].name}
+                Album: ${dataTra[2].album.name}
+                Link: ${dataTra[2].href}
+                `)
+            } else {
+                //create a for loop to loop over each result in the data array limited to 5 per search as per 3rd search parameter above
+                for (var i = 0; i < dataTra.length; i++) {
+                    //create a variable for each desired data retrieved from search (artist, album, song, link)
+                    var artists = dataTra[i].artists.map(function (artist) {
+                        return artist.name
+                    });
+                    var album = dataTra[i].album.name;
+                    var song = dataTra[i].name;
+                    var link = dataTra[i].href;
+
+                    // result will display Artist(s), song's name, A preview link of the song, and album that the song is from
+                    console.log(`
+          ===============================  Result ${i+1}  ==============================      
 
                 Artist: ${artists}
                 Song: ${song}
                 Album: ${album}
                 Link: ${link}
-
                 `)
-             
-
+                }
             }
         })
-    // .then(function (response) {
-    //     console.log(response);
-    // })
 }
 
 // Use of Bands in Town API to execute Bands in Town Artist Events case command
@@ -82,7 +78,57 @@ var bandsInTownFn = function () {
 }
 
 // Use of OMDB API to execute movie search case command
-var omdbFn = function () {
+var omdbFn = function (searchTerm) {
+    //API defined search according to user input stored in searchTerm
+    var queryURL = 'http://www.omdbapi.com/?t=' + searchTerm + '&y=&plot=short&apikey=trilogy';
+    axios.get(queryURL).then(
+  function(response) {
+      var r = response.data;
+    console.log(`
+    ☆ ∩∩ （ • •）☆
+┏━∪∪━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+☆
+❝ ${searchTerm} Results。 ❞
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+* Title of Movie: ${r.Title}
+
+* Year the movie came out: ${r.Year}
+
+* IMDB Rating of the movie: ${r.imdbRating}
+
+* Rotten Tomatoes Rating of the movie: ${r.Ratings[1].Value}
+
+* Country where the movie was produced: ${r.Country}
+
+* Language of the movie: ${r.Language}
+
+* Plot of the movie: ${r.Plot}
+
+* Actors in the movie: ${r.Actors} 
+
+    `);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
 
 }
 
@@ -90,8 +136,6 @@ var omdbFn = function () {
 var dwisFn = function () {
 
 }
-
-
 
 //------------------------LIRI COMMAND EXECUTIONS------------------//
 
@@ -103,12 +147,12 @@ switch (liriCommand) {
         spotifyFn(searchTerm);
         break
     case "concert-this":
-        bandsInTownFn();
+        bandsInTownFn(searchTerm);
         break
     case "movie-this":
-        omdbFn();
+        omdbFn(searchTerm);
         break
     case "do-what-it-says":
-        dwisFn();
+        dwisFn(searchTerm);
         break
 }
